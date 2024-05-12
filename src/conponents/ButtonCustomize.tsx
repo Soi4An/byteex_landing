c9ca37525a90683fadead20bf8c962b871d4516d
Antonio S <soi4.toxa@ukr.net>
2024-05-12 21:20:24 +0300

    updated ButtonMain width

--- src/conponents/ButtonCustomize.tsx
+++ src/conponents/ButtonCustomize.tsx
@@ -5,7 +5,7 @@ type Props = {
 };
 
 const ButtonCustomize = ({ arrowImg }: Props) => (
-  <button className="flex gap-4 justify-center items-center w-full h-14 rounded bg-blue-text hover:bg-blue-text-hover text-white text-base transition-colors">
+  <button className="flex gap-4 justify-center items-center w-full max-w-96 min-w-52 h-14 rounded bg-blue-text hover:bg-blue-text-hover text-white text-base transition-colors">
     <p>{'Customize Your Outfit'}</p>
     
     {!!arrowImg &&  <Icon width={'23px'} url={arrowImg} />}
