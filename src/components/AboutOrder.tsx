bc66047e5daafe8121036514f8693f57bd949d79
Antonio S <soi4.toxa@ukr.net>
2024-05-14 15:04:07 +0300

    added ExttraButton for AboutOrder

--- src/components/AboutOrder.tsx
+++ src/components/AboutOrder.tsx
@@ -1,5 +1,6 @@
 import { getAboutOrderData } from "../api/getAboutOrderData";
 import Container from "./Container";
+import ExtraButtonAndStars from "./ExtraButtonAndStars";
 import TitleSubMain from "./TitleSubMain";
 
 const { sectionTitle, listOrderPoints } = getAboutOrderData();
@@ -7,12 +8,16 @@ const { sectionTitle, listOrderPoints } = getAboutOrderData();
 const AboutOrder = () => (
   <Container>
     <section>
-      <div>
+      <div className="flex justify-center">
         <TitleSubMain text={sectionTitle} />
       </div>
        
        <div>
         {/* <ListOrderPoints points={listOrderPoints} /> */}
+       </div>
+
+       <div>
+         <ExtraButtonAndStars />
        </div>
     </section>
   </Container>
