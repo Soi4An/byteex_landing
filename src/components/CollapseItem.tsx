879b71753c2367bc5d0be2a3d224980e773ba10f
Antonio S <soi4.toxa@ukr.net>
2024-05-19 10:09:46 +0300

    added smooth open in CollapseItem

--- src/components/CollapseItem.tsx
+++ src/components/CollapseItem.tsx
@@ -2,6 +2,7 @@ import { Collapse } from "react-collapse";
 import { TitleAndText } from "../types/TitleAndText";
 import { useState } from "react";
 import CollapseButton from "./CollapseButton";
+import "../App.css";
 
 type Props = {
   itemCollapse: TitleAndText;
@@ -11,16 +12,16 @@ function CollapseItem({ itemCollapse }: Props) {
   const { title, text } = itemCollapse;
   const [isOpened, setIsOpened] = useState<boolean>(false);
 
-  const clickOpen = () => setIsOpened((curr) => !curr);
+  const toggleOpen = () => setIsOpened((curr) => !curr);
 
   return (
     <div className="flex flex-col gap-1 md:gap-2">
       <div className="flex justify-between items-center">
         <h4 className="text-lg text-blue-text font-bold">{title}</h4>
-        <CollapseButton handlerClick={clickOpen} isOpen={isOpened} />
+        <CollapseButton handlerClick={toggleOpen} isOpen={isOpened} />
       </div>
 
-      <Collapse isOpened={isOpened}>
+      <Collapse isOpened={isOpened} theme={{ collapse: "height-transition" }}>
         <p>{text}</p>
       </Collapse>
     </div>
